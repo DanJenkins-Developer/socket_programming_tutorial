bc3f3bb701be81ec3edfe62eca9d92c0491725cc
Daniel Jenkins <djenkinsjg0617@gmail.com>
2022-09-18 15:20:26 -0500

    Change to user input for hostname/IP address

--- UDPClient.py
+++ UDPClient.py
@@ -1,8 +1,11 @@
 from socket import *
-serverName = '10.161.82.13'
+# Using IP of my machine for testing.
+# IP = '10.161.78.120'
+serverName = input(
+    'Enter the Host Name or IP Address of the server you wish to connect to: ')
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence:')
+message = input('Input lowercase sentence: ')
 clientSocket.sendto(message.encode(), (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
